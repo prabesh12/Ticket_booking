43e56e99478c86fb14f993eea7511235cbca74dc
prabesh12 <prabesholi8848@gmail.com>
2023-03-13 14:05:43 +0545

    created bottom nav bar

--- main.dart
+++ main.dart
@@ -5,38 +5,19 @@ void main() {
   runApp(const MyApp());
 }
 
-// enum AnimalType {
-//   cat,
-//   dog,
-//   rabbit;
-// }
-
-// void test(AnimalType animalType) {
-//   switch(animalType){
-//           case AnimalType.cat: print("cat");
-//           break;
-//           case AnimalType.dog : print("dog");
-//           break;
-//           case AnimalType.rabbit : print("rabbit");
-         
-//   }
-//   print("");
-// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // test(AnimalType.cat);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
        
         primarySwatch: Colors.blue,
       ),
-      home: const BottomNavigator(),
+      home: const BottomBar(),
     );
   }
 }
